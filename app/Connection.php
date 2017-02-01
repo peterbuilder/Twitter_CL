22d15b54130405ec19002e95924c784468535e7d
peterbuilder <peterbuilder@github.com>
2017-02-01 12:12:30 +0100

    Add configuration data and connection functions.

---
+++ app/Connection.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 31.01.17
+ * Time: 15:05
+ */
+class Connection
+{
+    private $mysqli;
+
+    public function __construct()
+    {
+        require 'config.php';
+
+        $this->mysqli = new mysqli($host, $user, $password, $database);
+
+        if($this->mysqli->connect_error)
+        {
+            die('Połączenie z bazą nieudane: '.$this->mysqli->connect_error);
+        } else
+            {
+                $this->mysqli->set_charset("utf8");
+                return true;
+            }
+    }
+
+    public function query($sql)
+    {
+        $result = $this->mysqli->query($sql);
+
+        if($result == false)
+        {
+            die("Błąd zapytania: ".$this->mysqli->error);
+        } else
+            {
+                return $result;
+            }
+    }
+}
---
+++ app/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 31.01.17
+ * Time: 14:57
+ */
+
+$host = 'localhost';
+$user = 'root';
+$password = 'coderslab';
+$database = 'twitter';
