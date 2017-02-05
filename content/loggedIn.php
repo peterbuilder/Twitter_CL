cf36103bdd0970a01307da9beb74c64383c56142
peterbuilder <peterbuilder@github.com>
2017-02-05 13:38:08 +0100

    Create basic content

---
+++ content/loggedIn.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 05.02.17
+ * Time: 12:04
+ */
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/mainPage.css">
+    <title>Twitter - STRONA GŁÓWNA</title>
+</head>
+<body>
+<div id="login">
+    <form action="userPage.php" method="post">
+        <button type="submit" formaction="userPage.php" formmethod="get" name="logeout">Wyloguj</button>
+    </form>
+</div>
+<div id="mainTweets">
+    <h1>Strona Główna</h1>
+    <?php
+        $connection = new Connection();
+        Tweet::showAllTweets($connection);
+    ?>
+</div>
+</body>
+</html>
