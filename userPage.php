e8d33063ee49b97882c5182a4245a326756acc0f
peterbuilder <peterbuilder@github.com>
2017-02-05 13:36:50 +0100

    Create temp page

--- userPage.php
+++ userPage.php
@@ -5,3 +5,39 @@
  * Date: 03.02.17
  * Time: 18:59
  */
+
+session_start();
+include 'app/Authorization.php';
+
+switch(true)
+{
+    case ($_SERVER['REQUEST_METHOD'] == "POST"):
+                                            include 'app/Connection.php';
+
+                                            $connection = new Connection();
+                                            Authorization::logIn($connection);
+
+                                            if(isset($_SESSION['logged']))
+                                            {
+                                                echo 'Zalogowano: ';
+                                                echo $_SESSION['logged'] . '<br>';
+                                                echo '<form>';
+                                                echo '<button type="submit" formaction="index.php">Powrót</button>';
+                                                echo '<form>';
+                                            } else
+                                                {
+                                                    echo 'Błąd logowania';
+                                                    echo '<form>';
+                                                    echo '<button type="submit" formaction="index.php">Powrót</button>';
+                                                    echo '<form>';
+                                                }
+                                            break;
+
+    case ($_SERVER['REQUEST_METHOD'] == "GET"):
+                                            Authorization::logeOut();
+                                            echo '<form>';
+                                            echo '<button type="submit" formaction="index.php">Powrót</button>';
+                                            echo '<form>';
+                                            break;
+}
+
