42e15028fd986534942021324c80140d8b048e95
peterbuilder <peterbuilder@github.com>
2017-02-05 13:40:45 +0100

    Change form action

--- content/loggedOut.php
+++ content/loggedOut.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <div id="login">
-    <form action="#" method="post">
+    <form action="userPage.php" method="post">
         <input type="text" name="username" placeholder="Nazwa użytkownika">
         <input type="password" name="password" placeholder="Hasło">
         <input type="submit" value="Zaloguj">
