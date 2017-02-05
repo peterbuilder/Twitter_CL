92970484c5aed96a695ec7f5485a4a71a1e81d2a
peterbuilder <peterbuilder@github.com>
2017-02-05 12:00:45 +0100

    Add function logIn

--- app/Authorization.php
+++ app/Authorization.php
@@ -6,8 +6,26 @@
  * Time: 16:27
  */
 
-
 class Authorization
 {
+    static public function logIn(Connection $connection)
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
+        $sql = "SELECT hashed_password FROM user WHERE username='$username'";
+        $result = $connection->query($sql);
+        $result = $result->fetch_assoc();
+        $hashedPassword = $result['hashed_password'];
+
+        if(password_verify($password, $hashedPassword))
+        {
+            $_SESSION['logged'] = $username;
+            return true;
+        } else
+            {
+                return false;
+            }
+    }
 }
+
